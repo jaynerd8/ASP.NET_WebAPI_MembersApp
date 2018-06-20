2cfcccb0567380bcf8203487f7317bdfffbacee3
unknown <j4yn3rd@gmail.com>
2018-06-21 00:05:29 +1200

    members controller updated

---
+++ MembersApp/Controllers/MembersController.cs
@@ -0,0 +1,40 @@
+﻿using MembersApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MembersApp.Controllers
+{
+    public class MembersController : ApiController
+    {
+        private Members members = Members.GetMembers();
+
+        public IEnumerable<Member> GetMembers()
+        {
+            return members.GetMember();
+        }
+
+        public Member GetMember(string id)
+        {
+            return members.FindById(id);
+        }
+
+        public bool AddMember(Member m)
+        {
+            return members.AddMember(m);
+        }
+
+        public bool RemoveMember(string id)
+        {
+            return members.RemoveMember(id);
+        }
+
+        public bool ModifyMemeber(Member m)
+        {
+            return members.ModifyMember(m);
+        }
+    }
+}
